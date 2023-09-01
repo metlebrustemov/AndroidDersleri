fecb1db71eee9999b0cfad61a675dc1d257dfc13
Metleb1996 <metlebrustemov1996@gmail.com>
2023-09-01 06:15:36 +0400

    BroadcastReceiver

---
+++ app/src/main/java/az/metleb/androiddersleri/MyReceiver2.java
@@ -0,0 +1,14 @@
+package az.metleb.androiddersleri;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver2 extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Custom Intent Detected.", Toast.LENGTH_LONG).show();
+    }
+}
