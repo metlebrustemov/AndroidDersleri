5731a1a7f99b7566becdb47a2ee0ec446965f14e
Metleb1996 <metlebrustemov1996@gmail.com>
2023-09-01 06:39:45 +0400

    ContentProviders

--- app/src/main/java/az/metleb/androiddersleri/MainActivity.java
+++ app/src/main/java/az/metleb/androiddersleri/MainActivity.java
@@ -2,13 +2,48 @@ package az.metleb.androiddersleri;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickAddName(View view) {
+// Add a new student record
+        ContentValues values = new ContentValues();
+        values.put(StudentsProvider.NAME,
+                ((EditText)findViewById(R.id.txtName)).getText().toString());
+        values.put(StudentsProvider.GRADE,
+                ((EditText)findViewById(R.id.txtGrade)).getText().toString());
+        Uri uri = getContentResolver().insert(
+                StudentsProvider.CONTENT_URI, values);
+        Toast.makeText(getBaseContext(),
+                uri.toString(), Toast.LENGTH_LONG).show();
+    }
+    public void onClickRetrieveStudents(View view) {
+// Retrieve student records
+        String URL = "content://az.metleb.androiddersleri.provider.College/students";
+        Uri students = Uri.parse(URL);
+        Cursor c = managedQuery(students, null, null, null, "name");
+        if (c.moveToFirst()) {
+            do{
+                Toast.makeText(this,
+                        c.getString(c.getColumnIndex(StudentsProvider._ID)) +
+                                ", " + c.getString(c.getColumnIndex( StudentsProvider.NAME)) +
+                                ", " + c.getString(c.getColumnIndex( StudentsProvider.GRADE)),
+                        Toast.LENGTH_SHORT).show();
+            } while (c.moveToNext());
+        }
     }
 }
